0e940c6e5d691147ee6246df69ed39f6d280cecf
bida24-167 <bida24-167@thuto.bac.ac.bw>
2025-06-06 02:06:20 +0200

    fnb

---
+++ src/main/java/com/example/futurebank/FutureBankApplication.java
@@ -0,0 +1,13 @@
+package com.example.futurebank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FutureBankApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FutureBankApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/futurebank/FutureBankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.futurebank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FutureBankApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
